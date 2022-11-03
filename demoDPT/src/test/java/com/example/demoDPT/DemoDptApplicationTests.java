ff7b8bf566689235bf06f7ae4ef3a664558477d6
snomiss <116875005+snomiss@users.noreply.github.com>
2022-11-03 14:23:58 +0100

    Add files via upload

---
+++ demoDPT/src/test/java/com/example/demoDPT/DemoDptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoDPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
