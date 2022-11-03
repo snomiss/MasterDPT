a7b964210a032525f6b4d7ef23a794a61e4b0e0c
snomiss <116875005+snomiss@users.noreply.github.com>
2022-11-03 15:50:51 +0100

    Add files via upload

---
+++ springbootDPT/src/main/java/com/example/springbootDPT/SpringbootDptApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootDPT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDptApplication.class, args);
+	}
+
+}
---
+++ springbootDPT/src/test/java/com/example/springbootDPT/SpringbootDptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootDPT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
